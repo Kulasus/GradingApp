cc23230e1af812b30c7283f8f6802bfacc5ed279
Lukáš Kondiolka <lukaskondiolka@seznam.cz>
2019-12-26 15:44:57 +0100

    Upgraded template for product + added user view

--- mysite/eshop/views.py
+++ mysite/eshop/views.py
@@ -1,20 +1,18 @@
 '''imports'''
 from django.shortcuts import render, get_object_or_404
-from .models import Product, Category, Review
+from .models import Product, Category, Review, User
 
 
 
 def index(request):
     '''home page view(index.html)'''
     newest_products_list = Product.objects.order_by('-created_at')[:10]
-    context = {'newest_products_list' : newest_products_list}
-    return render(request, 'eshop/index.html', context)
+    return render(request, 'eshop/index.html', {'newest_products_list' : newest_products_list})
 
 def categories(request):
     '''All categories view'''
     all_categories_list = Category.objects.all()
-    context = {'all_categories_list' : all_categories_list}
-    return render(request, 'eshop/categories.html', context)
+    return render(request, 'eshop/categories.html', {'all_categories_list' : all_categories_list})
 
 def category_detail(request, category_id):
     '''One exact category view'''
@@ -24,8 +22,7 @@ def category_detail(request, category_id):
 def products(request):
     '''All products view'''
     all_products_list = Product.objects.all()
-    context = {'all_products_list' : all_products_list}
-    return render(request, 'eshop/products.html', context)
+    return render(request, 'eshop/products.html', {'all_products_list' : all_products_list})
 
 def product_detail(request, product_id):
     '''One exact product view'''
@@ -35,6 +32,10 @@ def product_detail(request, product_id):
 def reviews(request):
     '''All newest reviews view'''
     all_reviews_list = Review.objects.order_by('-created_at')[:5]
-    context = {'all_reviews_list' : all_reviews_list}
-    return render(request, 'eshop/reviews.html', context)
+    return render(request, 'eshop/reviews.html', {'all_reviews_list' : all_reviews_list})
+
+def user_profile(request, user_id):
+    '''User profile view'''
+    user = get_object_or_404(User, id=user_id)
+    return render(request, 'eshop/user_profile.html', {'user':user})
     
