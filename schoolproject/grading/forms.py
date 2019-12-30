6dc395899a505a7544e205cfe22d4a342da125ba
Lukáš Kondiolka <lukas.kondiolka@tieto.com>
2019-12-30 12:58:00 +0100

    Added form for adding points

--- schoolproject/grading/forms.py
+++ schoolproject/grading/forms.py
@@ -10,3 +10,8 @@ class StudentLoginForm(forms.Form):
     '''Login for Student'''
     login = forms.CharField(label="Login", max_length=7)
     password = forms.CharField(label="Password", max_length=100)
+
+class AddPointsToStudentForm(forms.Form):
+    '''Form for adding points to students'''
+    login = forms.CharField(label="Login", max_length=7)
+    points = forms.IntegerField(label="Points")
